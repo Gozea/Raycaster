802b7593c03303844a126c047cb8811f2d9d7b67
= <raphael.bournet@u-psud.fr>
2022-08-19 01:55:52 +0200

    walls but fisheye

--- raycaster.cpp
+++ raycaster.cpp
@@ -9,12 +9,17 @@ using namespace Eigen;
 
 //window property
 const int width = 1024;
-const int height = 640;
+const int height = 520;
 
 float fov = M_PI/2;
-float nrays = 20;
+float zfov = M_PI/8;
+const int  nrays = 64;
+float drays[nrays];
 
-const int nWalls = 25;
+float zeye = 10;
+float zwall = 40;
+
+const int nWalls = 8;
 Vector4f walls[nWalls+4];
 
 struct character {
@@ -73,7 +78,7 @@ Vector2f intersect(Vector2f ray, Vector4f wall) {
 	//formula to get t (case with div by are if a wall is a point or ray is parallel with horizontal or vertical wall)
 	float den = (-C.y()*A.x()+C.x()*A.y());
 	float t = (den != 0) ? (-C.y()*(D.x()-B.x())+C.x()*(D.y()-B.y())) / den : -1;
-	//we need to compute u to ensure we intersect inside of the segment
+	//we need to compute u to ensure we don't intersect outside of the segment
 	float u = (den != 0) ? (-A.y()*(D.x()-B.x())+A.x()*(D.y()-B.y())) / den : -1;
 	Vector2f res = (t>=0 && u >= 0 && u <= 1) ? Vector2f(B.x()+A.x()*t, B.y()+A.y()*t) : Vector2f(-1, -1);
 	return res;
@@ -86,7 +91,6 @@ void rays(sf::RenderWindow& window) {
 		sf::Vertex ray[2];
 		//the direction of the ray
 		Vector2f dirRay = Vector2f(cos((-fov/2+i/nrays*fov)+hero.angle), sin((-fov/2+i/nrays*fov)+hero.angle));
-		cout << "dirray " << i << " " << dirRay.x() << " " << dirRay.y() << endl;
 		//intersect this ray with each wall
 		vector<Vector2f> intersects;
 		for (int w = 0 ; w < nWalls+4 ; w++) {
@@ -96,32 +100,53 @@ void rays(sf::RenderWindow& window) {
 		//take the intersected wall for which the distance is the shortest
 		Vector2f p;
 		float dist = width*height;
-		cout << "inter" << endl;
 		for (int w = 0 ; w < intersects.size() ; w++) {
-			cout << w << " " << intersects[w].x() << " " << intersects[w].y() << endl;
 			float d = sqrt(pow(intersects[w].x()-hero.x, 2) + pow(intersects[w].y()-hero.y, 2));
 			if (d < dist) {
 				p = intersects[w]; 
 				dist = d;
-				cout << w << " dist " << d << endl;
+				drays[(int)i] = dist;
 			}
 		}
-		//draw line between hero and chosen point (we'll at least intersect the border wall)
+		//create line between hero and chosen point (we'll at least intersect the border wall)
 		ray[0].position = sf::Vector2f(hero.x, hero.y);
 		ray[1].position = sf::Vector2f(p.x(), p.y());
 		ray[0].color = sf::Color::Red;
 		ray[1].color = sf::Color::Red;
-
+		//draw line in window
 		window.draw(ray, 2, sf::Lines);
 	}	
-
 }
+
+
+void fpsDraw(sf::RenderWindow& window) {
+	for(int i = 0 ; i < nrays ; i++) {
+		//correct fish eye effect
+		int da = abs(i-nrays/2)/nrays/2*fov/2;
+		float dist = drays[i]*cos(da);
+		//get top and bottom coordinates on the second screen
+		int bottom = height/2 - (height/2)*zeye/(dist*tan(zfov/2));
+		int top = height/2 + (height/2)*zwall/(dist*tan(zfov/2));
+		//draw rectangle
+		sf::RectangleShape rect(sf::Vector2f(width/nrays, top-bottom));
+		rect.setPosition(sf::Vector2f(i*width/nrays, height/2-top));
+		//color depends on distance
+		rect.setFillColor(sf::Color(255,255,255));
+		window.draw(rect);
+	}
+}
+
 
 int main() {
 
 	//SFML
 	// create the window (remember: it's safer to create it in the main thread due to OS limitations)
-        sf::RenderWindow window(sf::VideoMode(width, height), "Donut");
+        sf::RenderWindow window1(sf::VideoMode(width, height), "Raycaster");
+	sf::RenderWindow window2(sf::VideoMode(width, height), "FPS");
+
+	//window position
+	window1.setPosition(sf::Vector2i(0,0));
+	window2.setPosition(sf::Vector2i(0,height));
 
 	//create triangle
         sf::CircleShape triangle(20.f, 3);
@@ -132,37 +157,42 @@ int main() {
 	initWalls();
 
          // run the program as long as the window is open
-        while (window.isOpen())
+        while (window1.isOpen() && window2.isOpen())
         {
 		// check all the window's events that were triggered since the last iteration of the loop
                 sf::Event event;
-                while (window.pollEvent(event))
+                while (window1.pollEvent(event))
                 {
                     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {rotate(-0.1); triangle.rotate(-0.1*360/(2*M_PI));}
                     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {rotate(0.1); triangle.rotate(0.1*360/(2*M_PI));}
                     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {hero.x+=hero.mvspeed*hero.dx.x();hero.y+=hero.mvspeed*hero.dx.y();};
                     if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {hero.x-=hero.mvspeed*hero.dx.x();hero.y-=hero.mvspeed*hero.dx.y();};
                     // "close requested" event: we close the window
-                    if (event.type == sf::Event::Closed) window.close();
+                    if (event.type == sf::Event::Closed) {window1.close();window2.close();}
 
                 }
 
 	        // clear the window with black color
-                window.clear(sf::Color::Black);
+                window1.clear(sf::Color::Black);
+		window2.clear(sf::Color::Black);
 
 		//draw walls
-		drawWalls(window);
+		drawWalls(window1);
 
 		//draw triangle
 		triangle.setPosition((sf::Vector2f(hero.x, hero.y)));
 
 		//draw rays
-		rays(window);
+		rays(window1);
 
-		//window draw
-		window.draw(triangle);
+		//draw hero
+		window1.draw(triangle);
+
+		//draw fps window
+		fpsDraw(window2);
 
 	        //end of current frame 
-	        window.display();
+	        window1.display();
+		window2.display();
 	}	
 }
